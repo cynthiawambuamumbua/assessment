36f306911fed0be38666593697e34913d1a1b226
cynthiawambuamumbua <cynthiawambuamumbua@gmail.com>
2023-04-11 09:09:07 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,63 @@
+//Write a function that takes in a string and returns it when reversed
+let food = "eating"
+
+//Write a function that takes in the following array and consoles the target if it is found else
+null
+let num = [2,8,0,23,5,45,76]
+Target = 23
+function array(target){
+    if(left>right){
+        left=0;
+        right=num.length-1;
+        middle=num((left+right)/2)
+        i++
+        return num
+        
+    }else{
+        return null
+    }
+    
+}
+
+console.log(num)
+
+//Given years between 2000 and 2023, console all the leap years in the following
+//sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+let years=[2000,2023]
+switch(years){
+    case 1:
+    return "leapYear"
+    case 2:
+        return "not a leapYear"
+}
+console.log(years)
+
+//Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+//“Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+// let numbers=[0..toExponential100];
+// // for(num in numbers){
+// //     if(num/3===0){
+// //         console.log("Fizz")
+// //     } else{
+// //         numbers(num/5 ===0);
+// //     console.log("Buzz")
+
+// //     }
+// //         numbers(num/3&& num /5)
+// //         console.log("FizzBuzz")
+//     }
+
+   // Write a function that takes in an array of numbers and returns an array that has all
+//elements multiplied by 4.
+let numArray = [12,87,45,75,23,64,73]
+ const multiplied=numArray.forEach(numArray=>numArray*4)
+ console.log(multiplied)
+
+ //Write a function that takes in an array of strings and returns an array with every element
+//turned into a number
+//let nums = [“10”,”24”,”45”,”56”,”67”]
+let nums=[10,24,45,56,67]
+function nums(){
+    const nums=nums.forEach(Number)
+    console.log(nums)
+}
